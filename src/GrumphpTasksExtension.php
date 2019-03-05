76fb46851c774d8d72552bf665bc12c7a10fbd05
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-03-05 11:48:03 +0100

    Add a Grumphp task extensions that allows you to add extra tasks or skip some tasks.

---
+++ src/GrumphpTasksExtension.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace drupol\DrupalConventions;
+
+use GrumPHP\Extension\ExtensionInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Exception\RuntimeException;
+
+/**
+ * Grumphp extension that allows to:
+ *
+ * 1: define extra tasks
+ * 2: skip tasks
+ *
+ * This has been initially written by Antonio De Marco
+ * For the package openeuropa/code-review.
+ */
+class GrumphpTasksExtension implements ExtensionInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ContainerBuilder $container)
+    {
+        if ($container->hasParameter('extra_tasks')) {
+            $tasks = $container->getParameter('tasks');
+
+            foreach ($container->getParameter('extra_tasks') as $name => $value) {
+                if (isset($tasks[$name])) {
+                    throw new RuntimeException(
+                      sprintf("Cannot override already defined task '%s' in 'extra_tasks'", $name)
+                    );
+                }
+
+                $tasks[$name] = $value;
+            }
+
+            $container->setParameter('tasks', $tasks);
+        }
+
+      if ($container->hasParameter('skip_tasks')) {
+        $tasks = $container->getParameter('tasks');
+
+        foreach ($container->getParameter('skip_tasks') as $name => $value) {
+          unset($tasks[$name]);
+        }
+
+        $container->setParameter('tasks', $tasks);
+      }
+
+    }
+}
