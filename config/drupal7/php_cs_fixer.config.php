21bcb7078a819b25523f79877bb4f81db2e47098
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-05-12 12:02:40 +0200

    Update.

--- config/drupal7/php_cs_fixer.config.php
+++ config/drupal7/php_cs_fixer.config.php
@@ -2,4 +2,15 @@
 
 use drupol\PhpCsFixerConfigsDrupal\Config\Drupal7;
 
-return Drupal7::create();
+$config = new Drupal7();
+
+$rules = $config->getRules();
+
+$rules['header_comment'] = [
+    'comment_type' => 'PHPDoc',
+    'header' => trim(file_get_contents(__DIR__ . '/../../resource/header.txt')),
+    'location' => 'after_declare_strict',
+    'separate' => 'both',
+];
+
+return $config->setRules($rules);
--- config/drupal8/php_cs_fixer.config.php
+++ config/drupal8/php_cs_fixer.config.php
@@ -2,4 +2,15 @@
 
 use drupol\PhpCsFixerConfigsDrupal\Config\Drupal8;
 
-return Drupal8::create();
+$config = new Drupal8();
+
+$rules = $config->getRules();
+
+$rules['header_comment'] = [
+    'comment_type' => 'PHPDoc',
+    'header' => trim(file_get_contents(__DIR__ . '/../../resource/header.txt')),
+    'location' => 'after_declare_strict',
+    'separate' => 'both',
+];
+
+return $config->setRules($rules);
