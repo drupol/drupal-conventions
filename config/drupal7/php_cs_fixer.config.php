3de7a6be8df16395144daaf84ce2a212fc702155
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-06-02 07:28:09 +0200

    Do not enable automatic file header.

--- config/drupal7/php_cs_fixer.config.php
+++ config/drupal7/php_cs_fixer.config.php
@@ -2,15 +2,4 @@
 
 use drupol\PhpCsFixerConfigsDrupal\Config\Drupal7;
 
-$config = new Drupal7();
-
-$rules = $config->getRules();
-
-$rules['header_comment'] = [
-    'comment_type' => 'PHPDoc',
-    'header' => trim(file_get_contents(__DIR__ . '/../../resource/header.txt')),
-    'location' => 'after_declare_strict',
-    'separate' => 'both',
-];
-
-return $config->setRules($rules);
+return new Drupal7();
--- config/drupal8/php_cs_fixer.config.php
+++ config/drupal8/php_cs_fixer.config.php
@@ -2,15 +2,5 @@
 
 use drupol\PhpCsFixerConfigsDrupal\Config\Drupal8;
 
-$config = new Drupal8();
+return new Drupal8();
 
-$rules = $config->getRules();
-
-$rules['header_comment'] = [
-    'comment_type' => 'PHPDoc',
-    'header' => trim(file_get_contents(__DIR__ . '/../../resource/header.txt')),
-    'location' => 'after_declare_strict',
-    'separate' => 'both',
-];
-
-return $config->setRules($rules);
